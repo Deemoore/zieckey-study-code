e37f17c6fb7d96753a54cf018b30304b0f05ad7d
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2011-01-02 13:31:57 +0000

    new add

---
+++ trunk/StudySolution/OpenGL_Programming_Guide/Chapter2/main.cpp
@@ -0,0 +1,16 @@
+#include "lines.h"
+
+int main( int argc, char* argv[] )
+{ 
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
+    glutInitWindowSize( 500, 500 );
+    glutInitWindowPosition( 100, 100 );
+    glutCreateWindow( "Light" );
+    init();
+    glutDisplayFunc( &display );
+    glutReshapeFunc( &reshape );
+    glutMainLoop();
+
+    return 0;
+}
