162003b5b05f4379734479976a07756b872a3f4a
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2011-01-02 13:59:31 +0000

    chapter2

---
+++ trunk/StudySolution/OpenGL_Programming_Guide/Chapter2/common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <gl/glut.h>
+
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "glut32.lib")
