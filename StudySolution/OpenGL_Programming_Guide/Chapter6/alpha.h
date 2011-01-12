a723feaba328e9acaf668e3ad4b3a3b4e687d09f
zieckey <deemoore121@gmail.com>
2011-01-12 10:42:23 +0000

    alpha alpha3d blend

---
+++ StudySolution/OpenGL_Programming_Guide/Chapter6/alpha.h
@@ -0,0 +1,97 @@
+#include "common.h"
+
+static int leftFirst = GL_TRUE;
+static GLfloat alpha = 0.75;
+/* Initialize alpha blending function. */
+static void init( void )
+{
+	glEnable( GL_BLEND );
+	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+	glShadeModel( GL_FLAT );
+	glClearColor( 0.0, 0.0, 0.0, 0.0 );
+}
+
+static void drawLeftTriangle( void )
+{
+	/* draw yellow triangle on LHS of screen */
+	glBegin( GL_TRIANGLES );
+	glColor4f( 1.0, 1.0, 0.0, alpha );
+	glVertex3f( 0.1, 0.9, 0.0 );
+	glVertex3f( 0.1, 0.1, 0.0 );
+	glVertex3f( 0.7, 0.5, 0.0 );
+	glEnd();
+}
+static void drawRightTriangle( void )
+{
+	/* draw cyan triangle on RHS of screen */
+	glBegin( GL_TRIANGLES );
+	glColor4f( 0.0, 1.0, 1.0, alpha );
+	glVertex3f( 0.9, 0.9, 0.0 );
+	glVertex3f( 0.3, 0.5, 0.0 );
+	glVertex3f( 0.9, 0.1, 0.0 );
+	glEnd();
+}
+void display( void )
+{
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	if ( leftFirst )
+	{
+		drawLeftTriangle();
+		drawRightTriangle();
+	}
+	else
+	{
+		drawRightTriangle();
+		drawLeftTriangle();
+	}
+
+	glFlush();
+}
+void reshape( int w, int h )
+{
+	glViewport( 0, 0, ( GLsizei ) w, ( GLsizei ) h );
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+
+	if ( w <= h )
+	{
+		gluOrtho2D( 0.0, 1.0, 0.0, 1.0 * ( GLfloat )h / ( GLfloat )w );
+	}
+	else
+	{
+		gluOrtho2D( 0.0, 1.0 * ( GLfloat )w / ( GLfloat )h, 0.0, 1.0 );
+	}
+}
+void keyboard( unsigned char key, int x, int y )
+{
+	switch ( key )
+	{
+		case 't':
+		case 'T':
+			leftFirst = !leftFirst;
+			glutPostRedisplay();
+            break;
+        case '+':
+            alpha += 0.05;
+            if ( alpha > 1.0 )
+            {
+                alpha = 1.0;
+            }
+            glutPostRedisplay();
+            break;
+        case '-':
+            alpha -= 0.05;
+            if ( alpha < 0.0 )
+            {
+                alpha = 0.0;
+            }
+            glutPostRedisplay();
+            break;
+		case 27: /* Escape key */
+			exit( 0 );
+			break;
+		default:
+			break;
+	}
+}
