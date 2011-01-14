c44eb451a431b9d7a186517377f0f388698018f4
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2011-01-14 04:25:11 +0000

    添加 抗锯齿 相关代码

--- trunk/StudySolution/OpenGL_Programming_Guide/Chapter6/alpha.h
+++ trunk/StudySolution/OpenGL_Programming_Guide/Chapter6/alpha.h
@@ -95,3 +95,17 @@ void keyboard( unsigned char key, int x, int y )
 			break;
 	}
 }
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(200, 200);
+    glutCreateWindow(argv[0]);
+    init();
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
