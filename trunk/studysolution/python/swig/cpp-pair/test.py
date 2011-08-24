2535e432923a5498b06bda270ef68b4613a39bfa
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2011-08-24 03:36:37 +0000

    [REV]cpp-pair test ok, but time-example test failed

---
+++ trunk/studysolution/python/swig/cpp-pair/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import pair 
+
+a = pair.pairii(2,3)
+print a.first
+print a.second
+
---
+++ trunk/studysolution/python/swig/time-example/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#
+#test failed. cannot work
+#
+
+
+import example 
+
+print example.fact(2)
