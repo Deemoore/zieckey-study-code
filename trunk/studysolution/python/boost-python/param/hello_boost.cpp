74e20abe148b837ae6367302d66b444d8477c3a8
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2011-09-05 01:59:23 +0000

    [REV] newly add.

---
+++ trunk/studysolution/python/boost-python/param/hello_boost.cpp
@@ -0,0 +1,10 @@
+#include <boost/python.hpp>
+
+#include "hello.h"
+
+BOOST_PYTHON_MODULE(hello_ext)
+{
+    using namespace boost::python;
+    def("greet", greet);
+    def("fill", fill);
+}
---
+++ trunk/studysolution/python/boost-python/param/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+import hello_ext 
+
+print hello_ext.greet()
+
+result = hello_ext.Result();
+fill( result )
+print result.a;
+print result.b;
+        
