058ed7395c2872840695b4b38948b84fbfd35a86
zieckey <deemoore121@gmail.com>
2012-02-05 09:38:07 +0000

    添加同步派发

---
+++ nm/http/copy.bat
@@ -0,0 +1,9 @@
+set TopDir=E:\New-UMS\trunk\nm
+
+set SrcDir=.
+copy  %SrcDir%\"Pocket PC 2003 (ARMV4)"\Release\http.lib %TopDir%\3rdparty\lib\release
+copy  %SrcDir%\"Pocket PC 2003 (ARMV4)"\Debug\http.lib %TopDir%\3rdparty\lib\debug
+
+copy  %SrcDir%\include\*.h %TopDir%\http\include
+copy  %SrcDir%\..\net\include\*.h %TopDir%\net\include
+pause
