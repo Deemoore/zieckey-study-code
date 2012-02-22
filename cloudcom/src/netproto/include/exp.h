27fc5c6e4268c9c506e39a5b34061860ba305bf8
zieckey <deemoore121@gmail.com>
2012-02-22 13:37:22 +0000

    udpate

--- cloudcom/src/netproto/include/exp.h
+++ cloudcom/src/netproto/include/exp.h
@@ -9,16 +9,8 @@
 H_LINK_LIB( "osl");
 #endif
 
-#include "netproto/include/malloc_micro.h"
-#include "netproto/include/string_ext.h"
-#include "netproto/include/object.h"
-#include "netproto/include/string_util.h"
 #include "netproto/include/data_stream.h"
 
-
-#if H_PROVIDE_LOG_SYSTEM
-#include "netproto/include/log_manager.h"
-#endif
 
 #endif //#ifndef QOSLIB_ALL_EXPORT_H_
 
