78135908c2ff7a84506ca8b8dc9e3c51983c53f4
zieckey <deemoore121@gmail.com>
2012-02-23 15:54:14 +0000

    Add unit test of IDEA, Simple RSA, Openssl RSA

--- cloudcom/src/netproto/include/npp_config.h
+++ cloudcom/src/netproto/include/npp_config.h
@@ -8,33 +8,64 @@
 
 namespace npp
 {
-    typedef std::map<int, IDEA> IDEAMap;
-#ifdef H_PROVIDE_RSA
-    typedef std::map<int, OpenSSLRSA* > OpenSSLRSAMap;
-#endif
-
-    typedef std::map<int, SimpleRSA> SimpleRSAMap;
 
     class _EXPORT_NETPROTO NppConfig
     {
+        typedef std::map<int, IDEA>         IDEAMap;
+        typedef std::map<int, SimpleRSA>    SimpleRSAMap;
+#ifdef H_PROVIDE_RSA
+        typedef std::map<int, OpenSSLRSA > OpenSSLRSAMap;
+#endif
     public:
+        NppConfig(bool support_plain, bool sign_pack, bool verify_sign);
+
         bool AddIdeaKey(int key_no, unsigned char key[16]);
 
+        bool AddOpenSSLRSAKey(int key_no, 
+            const unsigned char* private_key, const size_t private_key_len,
+            const unsigned char* public_key, const size_t public_key_len );
+
+        bool AddSimpleRSAKey(int key_no, 
+            const unsigned char* private_key, const size_t private_key_len,
+            const unsigned char* public_key, const size_t public_key_len );
+
+        bool IsSupportPlainData() const;
+
+        bool IsSignData() const;
+
+        bool IsVerifySign() const;
+
+        OpenSSLRSA* GetOpenSSLRSA(int index);
+
+        SimpleRSA* GetSimpleRSA(int index);
+
+        IDEA* GetIDEA(int index);
     private:
-        bool support_plain;
-        bool sign_pack;
-        bool verify_sign;
+        bool support_plain_;
+        bool sign_pack_;
+        bool verify_sign_;
 
-        IDEAMap idea_encrypt_map_;
-        IDEAMap idea_decrypt_map_;
-
-#ifdef H_PROVIDE_RSA
-        OpenSSLRSAMap rsa_sign_map_;
-        OpenSSLRSAMap rsa_verify_map_;
-#endif
-        
+        IDEAMap       idea_map_;
         SimpleRSAMap  slrsa_map_;
+#ifdef H_PROVIDE_RSA
+        OpenSSLRSAMap rsa_map_;
+#endif
     };
+
+    inline bool NppConfig::IsSupportPlainData() const 
+    {
+        return support_plain_;
+    }
+
+    inline bool NppConfig::IsSignData() const
+    {
+        return sign_pack_;
+    }
+
+    inline bool NppConfig::IsVerifySign() const
+    {
+        return verify_sign_;
+    }
 }//end of namespace npp
 
 
