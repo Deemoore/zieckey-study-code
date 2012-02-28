1063de4153efe19e618829e78296bbd07b66064e
zieckey <deemoore121@gmail.com>
2012-02-28 12:37:50 +0000

    [REV]refactor move  H_FREE to *.cc

--- cloudcom/src/netproto/include/data_stream.h
+++ cloudcom/src/netproto/include/data_stream.h
@@ -5,10 +5,7 @@
 
 namespace npp
 {
-#define H_ALLOC( size )               ::malloc( (size) )
-#define H_ALLOC_NAME( size, name )    ::malloc( (size) )
-#define H_ALLOC_NAME_FILE( size, name, file_name, line_no )    ::malloc( (size) )
-#define H_FREE( p )       if(p){ ::free( (p) ); p = 0;}
+
 
     //! The class encapsulates data stream in memory.
     class _EXPORT_NETPROTO MemoryDataStream
@@ -499,20 +496,6 @@ namespace npp
 
     }
 
-    inline MemoryDataStream::MemoryDataStream( int nBufferSize ) : m_bSelfCreate( true )
-        , m_nSizeBuffer( nBufferSize )
-        , m_nPosWrite( 0 )
-        , m_nPosRead( 0 )
-        , m_nSize( 0 )
-        , m_nStates( 0 )
-    {
-        m_pData = ( u8* )H_ALLOC_NAME( m_nSizeBuffer, "MemoryDataStream" );
-
-        if ( !m_pData )
-        {
-            m_nSizeBuffer = 0;
-        }
-    }
     //! Construct with outside memory and size.
     inline MemoryDataStream::MemoryDataStream( npp::u8* pData, int nBufferSize, bool bDestroy )
         : m_pData( pData )
@@ -523,14 +506,6 @@ namespace npp
         , m_nSize( nBufferSize )
         , m_nStates( 0 )
     {
-    }
-
-    inline MemoryDataStream::~MemoryDataStream()
-    {
-        if ( m_pData && m_bSelfCreate )
-        {
-            H_FREE(m_pData);
-        }
     }
 
     inline bool MemoryDataStream::isReadBad() const
@@ -876,80 +851,10 @@ namespace npp
         return *this;
     }
 
-    inline bool MemoryDataStream::appendSize( u32 nSizeToAdd )
-    {
-        u32 nNewSize = m_nSize + nSizeToAdd + 1;
-
-        // only if buffer is no sufficient, we reallocate it.
-        if ( nNewSize > ( u32 )m_nSizeBuffer )
-        {
-            u32 nNewBufferSize = nNewSize + ( nNewSize >> 1 );
-
-            u8* pNewBuf = ( u8* )H_ALLOC_NAME( nNewBufferSize, "MemoryDataStream" );
-
-            if ( !pNewBuf )
-            {
-                setStates( S_READ_BAD | S_WRITE_BAD );
-                return false;
-            }
-
-            if ( m_pData )
-            {
-                memcpy( pNewBuf, m_pData, m_nSizeBuffer );
-            }
-
-
-            if ( m_bSelfCreate )
-            {
-                H_FREE(m_pData);
-            }
-
-            m_pData       = pNewBuf;
-            m_nSizeBuffer = nNewBufferSize;
-            m_bSelfCreate = true;
-        }
-
-        return true;
-    }
 
     inline bool MemoryDataStream::setBufferSize( u32 new_size )
     {
         return reserve( new_size );
-    }
-
-
-    inline bool MemoryDataStream::reserve( u32 new_size )
-    {
-        if ( new_size > m_nSizeBuffer )
-        {
-            u8* pNewBuf = ( u8* )H_ALLOC_NAME( new_size, "MemoryDataStream" );
-
-            if ( !pNewBuf )
-            {
-                setStates( S_READ_BAD | S_WRITE_BAD );
-                return false;
-            }
-
-            m_nSizeBuffer = new_size;
-
-            if ( m_pData )
-            {
-                memcpy( pNewBuf, m_pData, m_nSize );
-            }
-
-
-            if ( m_bSelfCreate )
-            {
-                //means the old buffer is created by myself, we need free it
-                H_FREE(m_pData);
-            }
-
-
-            m_pData = pNewBuf;
-            m_bSelfCreate = true;
-        }
-
-        return true;
     }
 
     template< typename T>
--- cloudcom/src/netproto/src/data_stream.cc
+++ cloudcom/src/netproto/src/data_stream.cc
@@ -10,13 +10,107 @@
 
 namespace npp
 {
-
+#define H_ALLOC( size )               ::malloc( (size) )
+#define H_ALLOC_NAME( size, name )    ::malloc( (size) )
+#define H_ALLOC_NAME_FILE( size, name, file_name, line_no )    ::malloc( (size) )
+#define H_FREE( p )       if(p){ ::free( (p) ); p = 0;}
 
     //////////////////////////////////////////////////////////////////////////
     //                         MemoryDataStream
     //////////////////////////////////////////////////////////////////////////
+    MemoryDataStream::MemoryDataStream( int nBufferSize ) : m_bSelfCreate( true )
+        , m_nSizeBuffer( nBufferSize )
+        , m_nPosWrite( 0 )
+        , m_nPosRead( 0 )
+        , m_nSize( 0 )
+        , m_nStates( 0 )
+    {
+        m_pData = ( u8* )H_ALLOC_NAME( m_nSizeBuffer, "MemoryDataStream" );
 
-    
+        if ( !m_pData )
+        {
+            m_nSizeBuffer = 0;
+        }
+    }
+
+    MemoryDataStream::~MemoryDataStream()
+    {
+        if ( m_pData && m_bSelfCreate )
+        {
+            H_FREE(m_pData);
+        }
+    }
+
+    bool MemoryDataStream::appendSize( u32 nSizeToAdd )
+    {
+        u32 nNewSize = m_nSize + nSizeToAdd + 1;
+
+        // only if buffer is no sufficient, we reallocate it.
+        if ( nNewSize > ( u32 )m_nSizeBuffer )
+        {
+            u32 nNewBufferSize = nNewSize + ( nNewSize >> 1 );
+
+            u8* pNewBuf = ( u8* )H_ALLOC_NAME( nNewBufferSize, "MemoryDataStream" );
+
+            if ( !pNewBuf )
+            {
+                setStates( S_READ_BAD | S_WRITE_BAD );
+                return false;
+            }
+
+            if ( m_pData )
+            {
+                memcpy( pNewBuf, m_pData, m_nSizeBuffer );
+            }
+
+
+            if ( m_bSelfCreate )
+            {
+                H_FREE(m_pData);
+            }
+
+            m_pData       = pNewBuf;
+            m_nSizeBuffer = nNewBufferSize;
+            m_bSelfCreate = true;
+        }
+
+        return true;
+    }
+
+    bool MemoryDataStream::reserve( u32 new_size )
+    {
+        if ( new_size > m_nSizeBuffer )
+        {
+            u8* pNewBuf = ( u8* )H_ALLOC_NAME( new_size, "MemoryDataStream" );
+
+            if ( !pNewBuf )
+            {
+                setStates( S_READ_BAD | S_WRITE_BAD );
+                return false;
+            }
+
+            m_nSizeBuffer = new_size;
+
+            if ( m_pData )
+            {
+                memcpy( pNewBuf, m_pData, m_nSize );
+            }
+
+
+            if ( m_bSelfCreate )
+            {
+                //means the old buffer is created by myself, we need free it
+                H_FREE(m_pData);
+            }
+
+
+            m_pData = pNewBuf;
+            m_bSelfCreate = true;
+        }
+
+        return true;
+    }
+
     //------------------------------------------------------------
     void MemoryDataStream::toText()
     {
