e1afc1fbe03ec87b7da925d3b5648d1277f409ca
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2012-03-04 00:47:33 +0000

    [REV]Add std_string_resize test

--- trunk/cloudcom/src/osl/test/trivial_test.cc
+++ trunk/cloudcom/src/osl/test/trivial_test.cc
@@ -1,5 +1,6 @@
 
 #include "osl/include/test_common.h"
+#include "osl/include/exp.h"
 
 #include <iostream>
 #include <string>
@@ -13,11 +14,26 @@ namespace
         const char* s2 = "mid=731d8949087ebf6cb8b85a49779cab51\r\n";
         s.append(s1);
         s.append(s2);
+        char buf[1024] = {};
+        snprintf(buf, sizeof(buf), "%s%s", s1, s2);
         H_TEST_ASSERT(s.size() == strlen(s1) + strlen(s2));
+        H_TEST_ASSERT(s == buf);
+    }
+
+    void test_std_string_resize()
+    {
+        std::string s = "1234567890";
+        s.resize(10);
+        H_TEST_ASSERT(s == "1234567890");
+        s.resize(5);
+        H_TEST_ASSERT(s == "12345");
+        s.resize(20);
+        H_TEST_ASSERT(strncmp(s.data(), "12345", 5) == 0);
     }
 }
 
-TEST_UNIT(trivial_test)
+TEST_UNIT(trivial_std_string_test)
 {
     test_std_string_append();
+    test_std_string_resize();
 }
