509191dd2ef82024ea16f8d745a65e612318d808
zieckey <deemoore121@gmail.com>
2012-03-05 07:00:04 +0000

    [REV]Add more test

--- cloudcom/src/osl/test/trivial_test.cc
+++ cloudcom/src/osl/test/trivial_test.cc
@@ -24,11 +24,32 @@ namespace
     {
         std::string s = "1234567890";
         s.resize(10);
+        char* p10 = &s[0];
         H_TEST_ASSERT(s == "1234567890");
         s.resize(5);
+        char* p5 = &s[0];
         H_TEST_ASSERT(s == "12345");
-        s.resize(20);
+        s.resize(8);
+        char* p8 = &s[0];
+        H_TEST_ASSERT(p8 == p5 && p5 == p10);
         H_TEST_ASSERT(strncmp(s.data(), "12345", 5) == 0);
+        s.resize(100);
+        char* p100 = &s[0];
+        H_TEST_ASSERT(strncmp(s.data(), "12345", 5) == 0);
+        H_TEST_ASSERT(p100 != p8);
+    }
+
+    void test_std_string_assign()
+    {
+        unsigned char key[] = {0x30, 0x81, 0x00, 0x02, 0x81, 0x81, 0x00, 0xe2, 0xc7, 0x68, 0x22, 0x2d, 0x8d, 0xbd, 0xb5, 0x0e};
+        std::string s1;
+        std::string s2;
+        s1 = std::string((char*)key, sizeof(key));
+        s2.assign((char*)key, sizeof(key));
+        H_TEST_ASSERT(s1.size() == sizeof(key));
+        H_TEST_ASSERT(s2.size() == sizeof(key));
+        H_TEST_ASSERT(s1 == s2);
+        H_TEST_ASSERT(memcmp(s1.data(), s2.data(), sizeof(key)) == 0);
     }
 }
 
@@ -36,4 +57,5 @@ TEST_UNIT(trivial_std_string_test)
 {
     test_std_string_append();
     test_std_string_resize();
+    test_std_string_assign();
 }
