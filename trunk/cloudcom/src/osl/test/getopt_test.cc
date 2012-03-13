0b8e9589792807f69aa7c37adf3e8d9ec35f9f44
zieckey <zieckey@ae56b796-9e8b-0566-aabf-05d7c2e0f773>
2012-03-13 14:47:22 +0000

    [REV]添加 getopt 在windows 平台，测试失败

---
+++ trunk/cloudcom/src/osl/test/getopt_test.cc
@@ -0,0 +1,96 @@
+
+#include "osl/include/test_common.h"
+#include "osl/include/getopt.h"
+#include "osl/include/platform_micros.h"
+
+#include <iostream>
+
+namespace
+{
+    void help()
+    {
+        printf(
+            "getopt test programn"
+            "Usage: test [OPTION] [INPUT]\n"
+            "   INPUT           set input filename (doesn't do anything)\n"
+            "   -h              help menu (this screen)\n"
+            "   -i              interactive mode (doesn't do anything)\n"
+            "   -f              force mode (doesn't do anything)\n"
+            "   -r              recursive mode (doesn't do anything)\n"
+            "   -v[level]       set verbosity level (5 is default; doesn't do anything)\n"
+            "   -o filename     set output filename (doesn't do anything)\n"
+            );
+    }
+
+
+
+#define FLAG_INTERACT   0x0001        
+#define FLAG_FORCE      0x0002        
+#define FLAG_RECURSIVE  0x0004        
+
+    int flags = 0;                        
+    int verbose = 5;                      
+    const char* in_fname = NULL;          
+    const char* out_fname = NULL;         
+
+    void getopt_test()
+    {
+        const char* argv[] = {
+            "./test_program",
+            "-f",
+            "-i",
+            "-r",
+            "-h",
+            "-v", 
+            "3",
+            "fff"
+        };
+        int argc = H_ARRAYSIZE(argv);
+        
+        while(1) 
+        {
+            int c = getopt(argc, const_cast<char**>(argv), "-ifrhv::o:");
+            if(c == -1) break;
+            switch(c) 
+            {
+            case 'i': 
+                flags |= FLAG_INTERACT; 
+                break;
+            case 'f': 
+                flags |= FLAG_FORCE; 
+                break;
+            case 'r': 
+                flags |= FLAG_RECURSIVE; 
+                break;
+            case 'h': 
+                help(); 
+                exit(0);
+            case 'v': 
+                verbose = atoi(optarg); 
+                break;
+            case 'o': 
+                out_fname = optarg; 
+                break;
+            case 1 : 
+                in_fname = optarg; 
+                break;
+            default:
+                printf("Option '%c' (%d) with '%s'\n", c, c, optarg);
+                break;
+            }
+        }
+
+        H_TEST_ASSERT((flags & FLAG_INTERACT)  == FLAG_INTERACT);
+        H_TEST_ASSERT((flags & FLAG_FORCE)     == FLAG_FORCE);
+        H_TEST_ASSERT((flags & FLAG_RECURSIVE) == FLAG_RECURSIVE);
+        H_TEST_ASSERT(verbose == 3);
+        H_TEST_ASSERT(out_fname == NULL);
+        H_TEST_ASSERT(strcmp(in_fname, "fff") == 0);
+    }
+}
+
+TEST_UNIT(getopt_test)
+{
+    //getopt_test();
+    //TODO
+}
