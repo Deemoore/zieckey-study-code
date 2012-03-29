8a66f0da3ac68043e54622fee0c6d9a2680c797c
zieckey <deemoore121@gmail.com>
2012-03-29 14:54:38 +0000

    [REV]Add svndiff.sh shell

---
+++ scriptstudy/shell/svndiff.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ $# -ne 1 ];then
+        echo "Usage $0 [source_file_name]"
+        echo "For example: $0 process_logic.cc"
+        exit
+fi
+
+file_name=$1
+modify_file_name=${file_name}.modify
+base_file_name=${file_name}.base
+svn up
+version=`svn up` 
+version=`echo $version | awk '{print $3}' | awk -F. '{print $1}'`
+cp -rf $file_name $modify_file_name
+svn revert $file_name
+cp -rf $file_name $base_file_name
+cp $modify_file_name $file_name
+vimdiff $file_name $base_file_name
